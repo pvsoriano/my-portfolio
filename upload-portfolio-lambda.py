50db5f264f0b3cc44e916d67853212a32062bced
Paul Soriano <pvsoriano@me.com>
2019-06-19 23:08:34 -0500

    Updated lambda code to include SNS to notify on build success/fail.

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -6,22 +6,30 @@ import zipfile
 import mimetypes
 
 def lambda_handler(event, context):
-
-    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:372371530702:deployPortfolioTopic')
+   
+    try: 
+        s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+        
+        portfolio_bucket = s3.Bucket('portfolio.paulvsoriano.com')
+        build_bucket = s3.Bucket('buildportfolio.paulvsoriano.com')
+        
+        portfolio_zip = io.BytesIO()
+        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+        
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+        
+        print("Job done.")
+        topic.publish(Subject='Portfolio Deployed', Message='Portfolio deployed successfully.')
     
-    portfolio_bucket = s3.Bucket('portfolio.paulvsoriano.com')
-    build_bucket = s3.Bucket('buildportfolio.paulvsoriano.com')
-    
-    portfolio_zip = io.BytesIO()
-    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
-    
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+    except:
+        topic.publish(Subject='Portfolio Deploy Failed.', Message='Portfolio deployment failed.')
+        raise
     
     return "Done"
-
-
-
+    
