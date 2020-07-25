c61dacc57068a1d6fc2ba7eb9031c681f637011e
pvsoriano <pvsoriano@me.com>
2020-07-25 00:25:08 -0400

    another one

--- js/example-work.js
+++ js/example-work.js
@@ -57,17 +57,18 @@ class ExampleWorkBubble extends React.Component {
         return (
             <div className="section__exampleWrapper"
                 onClick={ (evt) => this.props.openModal(evt, example)}>
-            <div className="section__exampleWrapper">
-                <div className="section__example">
-                    <img alt={ example.image.desc }
-                        className="section__exampleImage"
-                        src={ example.image.src}/>
-                    <dl className="color--cloud">
+                <div className="section__exampleWrapper">
+                    <div className="section__example">
+                        <img alt={ example.image.desc }
+                            className="section__exampleImage"
+                            src={ example.image.src}/>
+                        <dl className="color--cloud">
                         <dt className="section__exampleTitle section__text--centered">
                             { example.title }
                         </dt>
-                        <dd></dd>
-                    </dl>
+                            <dd></dd>
+                        </dl>
+                    </div>
                 </div>
             </div>
         )
